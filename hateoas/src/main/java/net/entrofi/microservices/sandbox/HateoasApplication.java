3a477b5c42ac0d8c63690586595623fcfbd74426
Hasan COMAK <hasan.comak@monitise.com>
2017-04-19 15:44:57 +0300

    Basic OAuth Configuration

--- hateoas/src/main/java/net/entrofi/microservices/sandbox/HateoasApplication.java
+++ hateoas/src/main/java/net/entrofi/microservices/sandbox/HateoasApplication.java
@@ -7,11 +7,17 @@ import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
 public class HateoasApplication {
 
 	public static void main(String[] args) {
--- hateoas/src/main/java/net/entrofi/microservices/sandbox/model/Greeting.java
+++ hateoas/src/main/java/net/entrofi/microservices/sandbox/model/Greeting.java
@@ -10,13 +10,27 @@ package net.entrofi.microservices.sandbox.model;
 
 
 import org.springframework.hateoas.ResourceSupport;
+import org.springframework.util.StringUtils;
 
 public class Greeting extends ResourceSupport {
 
+
+    public static final String GREETING_BODY = "Hello";
+
     private final String greetingMessage;
 
+    public Greeting() {
+        this.greetingMessage = GREETING_BODY;
+    }
+
+
     public Greeting(final String name) {
-        greetingMessage = "Hello " + name;
+
+        if(!StringUtils.isEmpty(name)) {
+            greetingMessage = GREETING_BODY + " " + name;
+        }else {
+            greetingMessage = GREETING_BODY;
+        }
     }
 
 
