c214e2fcb6d33c7182699ad3db1f11f8a20d8818
Hasan Comak <entrofi@gmail.com>
2017-05-17 19:35:21 +0300

    Remove Auto-prepended copyright messages

--- flight-booking/flight-search/src/main/java/net/entrofi/microservices/sandbox/flightbooking/flightsearch/domain/repository/FlightSearchRepository.java
+++ flight-booking/flight-search/src/main/java/net/entrofi/microservices/sandbox/flightbooking/flightsearch/domain/repository/FlightSearchRepository.java
@@ -1,16 +1,6 @@
-/*
- * Copyright 2003-2016 Monitise Group Limited. All Rights Reserved.
- *
- * Save to the extent permitted by law, you may not use, copy, modify,
- * distribute or create derivative works of this material or any part
- * of it without the prior written consent of Monitise Group Limited.
- * Any reproduction of this material must contain this notice.
- */
+
 package net.entrofi.microservices.sandbox.flightbooking.flightsearch.domain.repository;
 
-/**
- * TODO add javadoc
- * Created on 11/05/2017.
- */
+
 public class FlightSearchRepository {
 }
---
+++ flight-booking/flight-search/src/main/java/net/entrofi/microservices/sandbox/flightbooking/flightsearch/env/FlightQuery.java
@@ -0,0 +1,9 @@
+
+package net.entrofi.microservices.sandbox.flightbooking.flightsearch.env;
+
+/**
+ * TODO add javadoc
+ * Created on 17/05/2017.
+ */
+public class FlightQuery {
+}
