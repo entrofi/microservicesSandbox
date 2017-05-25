9d95c070160adc9d4918055499dbacf1481dc72c
Hasan Comak <entrofi@gmail.com>
2017-05-25 11:15:16 +0300

    FMS, KBMS Consumption Improvements

---
+++ flight-booking/fms/src/main/java/net/entrofi/microservices/sandbox/fms/env/model/KBMSAirport.java
@@ -0,0 +1,43 @@
+package net.entrofi.microservices.sandbox.fms.env.model;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class KBMSAirport {
+
+    private final String code;
+
+    private final String name;
+
+    private final String status;
+
+    @JsonCreator
+    public KBMSAirport(@JsonProperty("code") String code,
+                       @JsonProperty("name") String name,
+                       @JsonProperty("status") String status) {
+        this.code = code;
+        this.name = name;
+        this.status = status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
--- flight-booking/fms/src/main/java/net/entrofi/microservices/sandbox/fms/env/service/FMSKBMSConsumerService.java
+++ flight-booking/fms/src/main/java/net/entrofi/microservices/sandbox/fms/env/service/FMSKBMSConsumerService.java
@@ -1,28 +1,73 @@
 package net.entrofi.microservices.sandbox.fms.env.service;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.entrofi.microservices.sandbox.fms.env.model.Airport;
 import net.entrofi.microservices.sandbox.fms.env.model.BaseServiceListResponse;
+import net.entrofi.microservices.sandbox.fms.env.model.KBMSAirport;
+import net.entrofi.microservices.sandbox.fms.env.model.KBMSDivision;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.hal.Jackson2HalModule;
+import org.springframework.hateoas.mvc.TypeConstrainedMappingJackson2HttpMessageConverter;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class FMSKBMSConsumerService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FMSKBMSConsumerService.class);
 
     private static final String KBMS_ENDPOINT = "http://localhost:8070";
 
     private RestTemplate restTemplate;
 
     public FMSKBMSConsumerService() {
-        this.restTemplate = new RestTemplate();
+        this.restTemplate = getRestTemplateWithHalMessageConverter();
+    }
+
+    private static RestTemplate getRestTemplateWithHalMessageConverter() {
+        RestTemplate restTemplate = new RestTemplate();
+        List<HttpMessageConverter<?>> existingConverters = restTemplate.getMessageConverters();
+        List<HttpMessageConverter<?>> newConverters = new ArrayList<>();
+        newConverters.add(getHalMessageConverter());
+        newConverters.addAll(existingConverters);
+        restTemplate.setMessageConverters(newConverters);
+        return restTemplate;
+    }
+
+    private static HttpMessageConverter getHalMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new Jackson2HalModule());
+        MappingJackson2HttpMessageConverter halConverter =
+                new TypeConstrainedMappingJackson2HttpMessageConverter(ResourceSupport.class);
+        halConverter.setSupportedMediaTypes(Arrays.asList(MediaTypes.HAL_JSON));
+        halConverter.setObjectMapper(objectMapper);
+        return halConverter;
     }
 
     public List<Airport> getAirports() {
-        final BaseServiceListResponse airportsResponse = restTemplate.getForObject(KBMS_ENDPOINT + "/airports", BaseServiceListResponse.class);
+        //TODO change this call to exchange with parameterizedtyperef...
+        final BaseServiceListResponse airportsResponse = restTemplate.getForObject(KBMS_ENDPOINT + "/airports",
+                BaseServiceListResponse.class);
 
         return airportsResponse.getEmbedded().getList();
     }
+
 
 }
