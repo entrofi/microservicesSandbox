9687e236bda65ebb87d50c2a3c5f12d122d278ab
Hasan Comak <entrofi@gmail.com>
2017-05-30 16:11:42 +0300

    Configuration Server & Repository Initialization
    - Configuration server added.
    - Config server client configurations are implemented for modules fms, kbms, flight-search, flight-booking

--- flight-booking/kbms/src/main/java/net/entrofi/microservices/sandbox/kbms/controller/GreetingController.java
+++ flight-booking/kbms/src/main/java/net/entrofi/microservices/sandbox/kbms/controller/GreetingController.java
@@ -2,6 +2,8 @@ package net.entrofi.microservices.sandbox.kbms.controller;
 
 
 import net.entrofi.microservices.sandbox.kbms.domain.model.Greeting;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -13,13 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greeting")
+@RefreshScope
 public class GreetingController {
+
+    @Value("${net.entrofi.microservices.property.refreshcheck:Default Check}")
+    private String propertyRefreshCheck = "default";
 
     @RequestMapping("/hello")
     public HttpEntity<Greeting> sayHello(@RequestParam(name = "name", defaultValue = "Spring Boot HATEOAS", required = false)
                                          String name) {
-        Greeting greeting = new Greeting(name);
-        greeting.add(ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(GreetingController.class).sayHello(name)).withSelfRel());
+        Greeting greeting = new Greeting(name + " " + propertyRefreshCheck);
+        greeting.add(ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(GreetingController.class).sayHello
+                (name + " " + propertyRefreshCheck)).withSelfRel());
         return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
     }
 }
