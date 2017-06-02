d6c99f56bf3d8ea3d804a6b085acf93f1a286c62
Hasan Comak <entrofi@gmail.com>
2017-06-02 16:32:52 +0300

    Add Landing Pages for the web site
    Change-Id: I24234969eeb6a05d45100e24ee4d627c74da14f2

--- flight-booking/web/src/main/java/net/entrofi/microservices/sandbox/flightbooking/web/Controller/DefaultsController.java
+++ flight-booking/web/src/main/java/net/entrofi/microservices/sandbox/flightbooking/web/Controller/DefaultsController.java
@@ -1,11 +1,17 @@
-package net.entrofi.microservices.sandbox.flightbooking.web.Controller;
+package net.entrofi.microservices.sandbox.flightbooking.web.controller;
 
 
+import net.entrofi.microservices.sandbox.flightbooking.web.domain.model.WebFlightSearchQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DefaultsController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultsController.class);
 
     @GetMapping("/")
     public String index() {
@@ -18,7 +24,10 @@ public class DefaultsController {
     }
 
     @GetMapping("/flights/search")
-    public String searchFlights() {
+    public String searchFlights(Model model) {
+        LOGGER.info("Rendering flight search page");
+        WebFlightSearchQuery webFlightSearchQuery = new WebFlightSearchQuery();
+        model.addAttribute("flightSearchQuery", webFlightSearchQuery);
         return "search-flight";
     }
 
