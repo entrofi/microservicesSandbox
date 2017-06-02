16d006643f8f747619579b66abf93e0996f22431
Hasan Comak <entrofi@gmail.com>
2017-06-02 16:30:45 +0300

    Bootstrap Date Picker & Bootstrap CSS Update
    Change-Id: I24006783941cb813ca8fbf8d4d813689f3ffd871

---
+++ flight-booking/web/src/main/resources/static/js/app/search-flight.js
@@ -0,0 +1,10 @@
+
+$(document).ready(
+    function() {
+        $("#flight-search-date-container .input-group.date").datepicker(
+            {
+                startDate: '0d'
+            }
+        )
+    }
+);
